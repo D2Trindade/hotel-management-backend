2f6d0be6a53218c01dde198b1a8920725f36e223
kelvya <kelvya.kt@gmail.com>
2023-01-26 10:45:23 -0300

    Criado o componente BannerVue

---
+++ src/components/BannerVue.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <div>
+            <img :src="url" :alt="alt" v-on:load="alterarImagem" class="w-100" id="homeBanner" />
+        </div>
+    </section>
+</template>
+
+<script>
+export default{
+    name: 'BannerVue',
+    data() {
+        return {
+            url: require(`@/assets/images/background/background-1.png`),
+            alt: 'Grand Royal Hotel'
+        }
+    },
+    methods: {
+        alterarImagem() {
+            let qtdImagens = 7;
+            let sorteio = parseInt(Math.random() * qtdImagens);
+            this.url = require(`@/assets/images/background/background-${sorteio + 1}.png`);
+        }
+    }
+}
+</script>
