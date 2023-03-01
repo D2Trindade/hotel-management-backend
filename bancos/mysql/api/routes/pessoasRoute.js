154dc9a6422dd1ff34cf70a8acf4483880b1e8b1
kelvya <kelvya.kt@gmail.com>
2023-03-01 13:57:29 -0300

    Adicionadas as rotas para funcionarios, usuarios e hospedes da model Pessoas

--- bancos/mysql/api/routes/pessoasRoute.js
+++ bancos/mysql/api/routes/pessoasRoute.js
@@ -3,11 +3,42 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
+// Rotas para todas as pessoas com as 3 regras (usuario, hospede, funcionario)
 router.get('/pessoas', PessoaController.listarPessoas)
 router.get('/pessoas/:id', PessoaController.listarPessoaPorID)
 router.post('/pessoas', PessoaController.cadastrarPessoa)
 router.post('/pessoas/:id/restaura', PessoaController.restaurarPessoa)
 router.put('/pessoas/:id', PessoaController.atualizarPessoa)
 router.delete('/pessoas/:id', PessoaController.excluirPessoa)
+
+
+
+//Rotas apenas para funcionários da tabela Pessoas
+router.get('/funcionarios', PessoaController.listarFuncionarios)
+router.get('/funcionarios/:id', PessoaController.listarFuncionarioPorID)
+router.post('/funcionarios', PessoaController.cadastrarFuncionario)
+// router.post('/funcionarios/:id/restaura', PessoaController.restaurarFuncionario)
+router.put('/funcionarios/:id', PessoaController.atualizarFuncionario)
+router.delete('/funcionarios/:id', PessoaController.excluirFuncionario)
+
+
+
+//Rotas apenas para usuários da tabela Pessoas
+router.get('/usuarios', PessoaController.listarUsuarios)
+router.get('/usuarios/:id', PessoaController.listarUsuarioPorID)
+router.post('/usuarios', PessoaController.cadastrarUsuario)
+// router.post('/usuarios/:id/restaura', PessoaController.restaurarUsuario)
+router.put('/usuarios/:id', PessoaController.atualizarUsuario)
+router.delete('/usuarios/:id', PessoaController.excluirUsuario)
+
+
+
+//Rotas apenas para hospedes da tabela Pessoas
+router.get('/hospedes', PessoaController.listarHospedes)
+router.get('/hospedes/:id', PessoaController.listarHospedePorID)
+router.post('/hospedes', PessoaController.cadastrarHospede)
+// router.post('/hospedes/:id/restaura', PessoaController.restaurarHospede)
+router.put('/hospedes/:id', PessoaController.atualizarHospede)
+router.delete('/hospedes/:id', PessoaController.excluirHospede)
 
 module.exports = router
