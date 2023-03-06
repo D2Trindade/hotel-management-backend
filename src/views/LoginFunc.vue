831fc473a7a664080fcdfa52ac06881c92837b33
kelvya <kelvya.kt@gmail.com>
2023-03-06 10:52:13 -0300

    Adicionada a funcionalidade de ler a senha criptografada do funcionário e comparar com a senha digitada

--- src/views/LoginFunc.vue
+++ src/views/LoginFunc.vue
@@ -4,10 +4,10 @@
     <div class="login-form">          
       <div class="sign-in-htm">              
         <div class="modalLogin__group">                  
-          <input placeholder="E-mail" id="loginAdmUser" type="email" class="input">              
+          <input placeholder="E-mail" id="loginAdmUser" type="email" class="input" v-model="readInput.email">              
         </div>              
         <div class="modalLogin__group">                  
-          <input placeholder="Senha" id="loginAdmPass" type="password" class="input" data-type="password">              
+          <input placeholder="Senha" id="loginAdmPass" type="password" class="input" data-type="password" v-model="readInput.password">              
         </div>              
         <div class="modalLogin__group">                  
           <input type="submit" class="button" value="Entrar" @click="realizaLoginAdm">              
@@ -18,54 +18,89 @@
 </template>
 
 <script>
+import { descriptografarSenha } from '@/assets/js/Criptografar'
+
 export default {
   name: 'AdmLogin',
+  data() {
+    return {
+      user: [],
+      readInput: {
+      email: "",
+      password: ""
+      },
+    }
+  },
   methods: {
     async realizaLoginAdm() {
-      let email = document.querySelector('#loginAdmUser').value
-      let password = document.querySelector('#loginAdmPass').value
-      
-      // Valida Formato
-      if (!this.validateEmail(email)) {
+      var usuarioSistema = {
+        email: this.readInput.email,
+        password: this.readInput.password
+      }
+      await this.lerUsuario(this.readInput.email)
+
+      if (descriptografarSenha(this.readInput.password, this.user)){
+        alert('Seja bem vindo(a)!')
+        this.$router.push('/admHome')
+      } else{
+        alert ('Usuário ou senha não cadastrados!')
         document.querySelector('#loginAdmUser').value = ''
-        alert ("Informar um email válido")
-        return;
+        document.querySelector('#loginAdmPass').value = '' 
+        this.$router.push('/')
       }
+      console.log(`usuarioSistema: email ${usuarioSistema.email} senha: ${usuarioSistema.password}`)
+      console.log(`this.user: ${this.user}`)
 
-      // Busca as pessoas incluídas no DB
-      const conexao = require('@/assets/js/ConexaoAPI.js')
-      let pessoas = await conexao.getAPI('/pessoas')
-      for(let i=0; i<pessoas.length; i++) {
-        // Busca o usuário pelo email && verifica senha
-        if(pessoas[i].email == email && pessoas[i].password == password) {
-          // Verifica se login pertence a um funcionário
-          if (pessoas[i].role != 'funcionario') {
-            alert('Acesso apenas para funcionários!')
-            document.querySelector('#loginAdmUser').value = ''
-            document.querySelector('#loginAdmPass').value = '' 
-            this.$router.push('/')
-            return
-          }
 
-          let loginFunc = {
-            id: pessoas[i].id,
-            nome: pessoas[i].nome,
-            email: pessoas[i].email
-          }
-          localStorage.setItem('loginFunc', JSON.stringify(loginFunc))
-          this.$router.push('/admHome')
-          return
-        }
-      }
-      alert ("Usuário não encontrado!")
-      document.querySelector('#loginAdmUser').value = ''
-      document.querySelector('#loginAdmPass').value = ''
-      return;
+      // let email = document.querySelector('#loginAdmUser').value
+      // let password = document.querySelector('#loginAdmPass').value
+      
+      // // Valida Formato
+      // if (!this.validateEmail(email)) {
+      //   document.querySelector('#loginAdmUser').value = ''
+      //   alert ("Informar um email válido")
+      //   return;
+      // }
+
+      // // Busca as pessoas incluídas no DB
+      // const conexao = require('@/assets/js/ConexaoAPI.js')
+      // let pessoas = await conexao.getAPI('/pessoas')
+      // for(let i=0; i<pessoas.length; i++) {
+      //   // Busca o usuário pelo email && verifica senha
+      //   if(pessoas[i].email == email && pessoas[i].password == password) {
+      //     // Verifica se login pertence a um funcionário
+      //     if (pessoas[i].role != 'funcionario') {
+      //       alert('Acesso apenas para funcionários!')
+      //       document.querySelector('#loginAdmUser').value = ''
+      //       document.querySelector('#loginAdmPass').value = '' 
+      //       this.$router.push('/')
+      //       return
+      //     }
+
+      //     let loginFunc = {
+      //       id: pessoas[i].id,
+      //       nome: pessoas[i].nome,
+      //       email: pessoas[i].email
+      //     }
+      //     localStorage.setItem('loginFunc', JSON.stringify(loginFunc))
+      //     this.$router.push('/admHome')
+      //     return
+      //   }
+      // }
+      // alert ("Usuário não encontrado!")
+      // document.querySelector('#loginAdmUser').value = ''
+      // document.querySelector('#loginAdmPass').value = ''
+      // return;
     },
     validateEmail(email) {
       var re = /\S+@\S+\.\S+/
 
       return re.test(email)
+    },
+    async lerUsuario(email) {
+      const api = require('@/assets/js/ConexaoAPI.js')
+      let usuario = await api.getAPI(`/pessoas/${email}`)
+      this.user = usuario
     }
   }
 }
